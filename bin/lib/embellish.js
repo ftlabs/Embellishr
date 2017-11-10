c1e16fe06f848a4e96f86bbcfdf2a5f230d93138
jdevt <33483633+jdevt@users.noreply.github.com>
2017-11-10 17:30:05 +0000

    Initial commit (Still wip, moving out old code)

---
+++ bin/lib/embellish.js
@@ -0,0 +1,110 @@
+const fetchContent = require('./fetchContent');
+const directly = require('./directly'); 
+const CAPI_CONCURRENCE = (process.env.hasOwnProperty('CAPI_CONCURRENCE'))? process.env.CAPI_CONCURRENCE : 4;
+
+//Create a word - number mapping of the months
+const monthMap = [ "January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December" ]
+.reduce((result, item, index) => {
+    result[index+1] = item;
+    return result;
+  }, {});
+
+/**
+ * Returns a dictionary of data for each month.
+ * @param {*} word 
+ * @param {*} year 
+ */ 
+function lookupWordByYear(word, year, params={}) {
+    let yearResponse = {};
+    return _fetchYear(word, year, params).then( searchResponses => {
+        for(let response of searchResponses) {
+            let month = new Date(response.params.month).getMonth() + 1;
+            yearResponse[monthMap[month]] = response.sapiObj.results;
+        }
+        return yearResponse
+    });
+}
+
+/**
+ * Create concurrent fetches for each month (uses directly)
+ */
+function _fetchYear(word, year, params={}) {
+    const wordLookupPromisers = []    
+    for(let i = 1; i <= 12; i++) {
+        wordLookupPromisers.push(() => lookupWordByMonth(word, year, i, params));
+    }
+    return directly(CAPI_CONCURRENCE, wordLookupPromisers)
+}
+
+/**
+ * Returns a summary of the year
+ * @param {*} word 
+ * @param {*} year 
+ */
+function yearSummary(word, year) {
+    let yearResponse = {
+        months: {},
+        total: null
+    };
+    return _fetchYear(word, year).then(searchResponses => {
+        let total = 0;
+        for(let response of searchResponses) {
+            let month = new Date(response.params.month).getMonth() + 1;
+            let monthString = [monthMap[month]];
+            let summary = _createSummary(response)
+            yearResponse.months[monthString] = summary;
+            total+= summary.count;            
+        }
+        yearResponse.total = total;
+        return yearResponse
+    });
+}
+
+/**
+ * Create a summary of each month, extracting key information
+ * @param {*} response 
+ * @param {*} peopleSize 
+ */
+function _createSummary(response, peopleSize = 10) {
+    const summary = {}
+    let results = response.sapiObj.results.pop();
+    let count = results.indexCount;
+    summary.count = count;
+    let people = [];
+    if(results.hasOwnProperty('facets') && (results.facets.length != 0)) {
+        people = results.facets.pop().facetElements;;
+    }
+    summary.people = people.length > peopleSize ? people.slice(0,peopleSize) : people;
+    return summary;
+}
+/**
+ * Looksup a word by month
+ * @param {*} word - Word to be searched
+ * @param {*} year - Year to be searched
+ * @param {*} month - Month (Number format)
+ * @private
+ */
+function lookupWordByMonth(word, year, month, queryParams={}) {
+    if(month > 12 || month < 1) {
+        throw new Error("Month must be given in a format between 01 - 12")
+    }
+    let beforeTime = new Date(year, month, 01).toISOString().replace('.000Z', 'Z');   
+    let afterTime = new Date(year, month, 01);
+    afterTime.setMonth(afterTime.getMonth() - 1);
+    afterTime = afterTime.toISOString().replace('.000Z', 'Z'); 
+    return fetchContent.searchWordBetweenRange(word, afterTime, beforeTime)
+        .catch(err => {
+            console.log(`Error: lookupWordByMonth failed to return for ${beforeTime} - ${afterTime}`);
+            return;
+    })
+}
+
+
+  
+
+module.exports = {
+    lookupWordByYear,
+    lookupWordByMonth,
+    yearSummary
+}
---
+++ index.js
@@ -0,0 +1,84 @@
+const  dotenv = require('dotenv').config({ silent : process.env.NODE_ENVIRONMENT === 'production'  });
+const   debug = require('debug')('correlations:index');
+const express = require('express');
+const path = require('path');
+const app = express();
+const fetchContent = require('./bin/lib/fetchContent');
+const embelish = require('./bin/lib/embellish');
+const v1v2 = require('./bin/lib/v1v2');
+const validateRequest = require('./bin/lib/check-token');
+
+var requestLogger = function(req, res, next) {
+    debug("RECEIVED REQUEST:", req.method, req.url);
+    next(); // Passing the request to the next handler in the stack.
+}
+
+app.use(requestLogger);
+
+// these routes do *not* have s3o
+app.use('/static', express.static('static'));
+
+//TODO: Fix this to properly use token
+const TOKEN = process.env.TOKEN;
+// if (! TOKEN ) {
+//   throw new Error('ERROR: TOKEN not specified in env');
+// }
+
+// these route *do* use s3o
+app.set('json spaces', 2);
+
+if (process.env.BYPASS_TOKEN !== 'true') {
+	app.use(validateRequest);
+}
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '/static/index.html'));
+ 
+  // res.render('home', {
+    
+  // });
+});
+
+app.get('/embelish/:year([0-9]{4})/:month([0-9]{2})/:word', (req, res) => {
+  const year = parseInt(req.params.year);
+  const month = parseInt(req.params.month);
+  const word = req.params.word;
+	embelish.lookupWordByMonth(req.params.word ,year, month).then(result => {
+    let response = {};
+    response['searchTerm'] = word;
+    response[year] = result;
+    res.json(response);
+  })
+});
+
+app.get('/embelish/:year([0-9]{4})/:word', (req, res) => {
+  const year = parseInt(req.params.year);
+  const word = req.params.word;
+  embelish.lookupWordByYear(word, year).then(results => {
+    let response = {};
+    response['searchTerm'] = word;
+    response[year] = results;
+    res.json(response);    
+  })
+});
+
+app.get('/embelish/summary/:year([0-9]{4})/:word', (req, res) => {
+  const year = parseInt(req.params.year);
+  const word = req.params.word;
+  embelish.yearSummary(word, year).then(results => {
+    let response = {};
+    response['searchTerm'] = word;
+    response[year] = results;
+    res.json(response);    
+  })  
+});
+
+//---
+
+function startListening(){
+	app.listen(process.env.PORT, function(){
+		console.log('Server is listening on port', process.env.PORT);
+	});
+}
+//---
+startListening();
